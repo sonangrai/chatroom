dcb4994abb866bd903ce25ab35517b1125d949bf
sonangrai <shonahangrae@gmail.com>
2021-12-15 12:35:20 +0545

    init chatroom

---
+++ build/main.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const socket_io_1 = require("socket.io");
+const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+//Creating an http server
+const server = http_1.default.createServer(app);
+//Implemented the server in the socket
+const io = new socket_io_1.Server(server);
+/**
+ * The connection event
+ */
+io.on("connection", (socket) => {
+    socket.on("sendmsg", (data) => {
+        io.emit("msgreceived", data);
+    });
+});
+/**
+ * Serving html
+ */
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.sendFile(path_1.default.join(__dirname + "/../public/index.html"));
+}));
+/**
+ * Main server
+ */
+const port = 4000;
+server.listen(port, () => {
+    console.log("App running in", port);
+});
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import { Server } from "socket.io";
+import http from "http";
+import path from "path";
+
+const app = express();
+
+//Creating an http server
+const server = http.createServer(app);
+//Implemented the server in the socket
+const io = new Server(server);
+
+/**
+ * The connection event
+ */
+io.on("connection", (socket) => {
+  socket.on("sendmsg", (data) => {
+    io.emit("msgreceived", data);
+  });
+});
+
+/**
+ * Serving html
+ */
+app.get("/", async (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/../public/index.html"));
+});
+
+/**
+ * Main server
+ */
+const port: Number = 4000;
+server.listen(port, () => {
+  console.log("App running in", port);
+});
