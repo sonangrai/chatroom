8939b9d9d2568531bbb2aa4e0b0f541821e14adf
sonangrai <shonahangrae@gmail.com>
2021-12-15 16:38:20 +0545

    frontend init

---
+++ chat/src/App.js
@@ -0,0 +1,13 @@
+import Global from "./componants/layout/Global.styled";
+import { Container } from "./componants/layout/Container.styled";
+
+function App() {
+  return (
+    <>
+      <Global />
+      <Container>Chat</Container>
+    </>
+  );
+}
+
+export default App;
---
+++ chat/src/componants/layout/Global.styled.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+*{
+    box-sizing: border-box;
+}
+body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Raleway', sans-serif;
+}
+`;
+
+export default Global;
