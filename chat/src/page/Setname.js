7fa9157b48fd945e06f1cc3da83938025b7f71fd
sonangrai <shonahangrae@gmail.com>
2021-12-15 17:14:19 +0545

    set name func

--- chat/src/page/Setname.js
+++ chat/src/page/Setname.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "../componants/layout/Container.styled";
 import {
   Box,
@@ -9,14 +9,25 @@ import {
 } from "../componants/Setname/Setname.styled";
 
 const Setname = () => {
+  const [name, setname] = useState("");
+
+  const onChange = (e) => {
+    setname(e.target.value);
+  };
+
+  const setName = (e) => {
+    e.preventDefault();
+    localStorage.setItem("name", name);
+  };
+
   return (
     <Container image="https://cdn.pixabay.com/photo/2014/07/01/15/40/balloon-381334_1280.png">
       <Box>
         <Title>
           <h2>Set your Nickname</h2>
         </Title>
-        <Form>
-          <Input />
+        <Form onSubmit={setName}>
+          <Input onChange={onChange} />
           <Button>Set name</Button>
         </Form>
       </Box>
