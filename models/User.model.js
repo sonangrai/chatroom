4b36a6bcfcf6cb96c6786bebdfe9aec6d58a9070
sonangrai <shonahangrae@gmail.com>
2021-12-20 17:03:12 +0545

    user modal added

---
+++ models/User.model.js
@@ -0,0 +1,21 @@
+const mongose = reuire("mongoose");
+
+/**
+ * The user modal
+ */
+const UserSchema = new mongose.Schema(
+  {
+    firstname: {
+      type: String,
+    },
+    lastname: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
