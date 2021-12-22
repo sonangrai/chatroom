930c5efce8b3456e1b8415c1588194f0db04d684
sonangrai <shonahangrae@gmail.com>
2021-12-22 16:50:47 +0545

    save msg inited

--- controllers/Message.js
+++ controllers/Message.js
@@ -6,5 +6,28 @@ const Message = require("../model/Message.model");
  * @param {*} res
  */
 exports.saveMsg = async (req, res) => {
-  res.send("msg saved");
+  const { message } = req.body;
+
+  let msgObj = new Message({
+    message: message,
+  });
+
+  try {
+    await msgObj.save();
+
+    //Creating success object
+    responseObj.msg = "Message added successfully";
+    responseObj.data = msgObj;
+    responseObj.status = 201;
+
+    //Emiting the event of success user added
+    req.io.emit("message-saved", responseObj);
+    res.send(responseObj);
+  } catch (error) {
+    //Creating success object
+    responseObj.msg = "Message adding failed";
+    responseObj.data = msgObj;
+    responseObj.status = 500;
+    res.send(responseObj);
+  }
 };
