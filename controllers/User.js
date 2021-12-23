f962931bd6a1b86016306c263bd7836e4163ffb3
sonangrai <shonahangrae@gmail.com>
2021-12-23 12:30:42 +0545

    new push

--- controllers/User.js
+++ controllers/User.js
@@ -8,23 +8,36 @@ exports.saveUser = async (req, res) => {
   const { fname, lname, email, avatar } = req.body;
 
   let userObj = new User({
-    fname: fname,
-    lname: lname,
+    firstname: fname,
+    lastname: lname,
     email: email,
     avatar: avatar,
   });
 
   try {
-    await userObj.save();
+    //Finding if they exist already
+    let userInDb = await User.findOne({ email: email });
+    if (userInDb) {
+      //Creating success object
+      responseObj.msg = "User login successfully";
+      responseObj.data = userInDb;
+      responseObj.status = 201;
 
-    //Creating success object
-    responseObj.msg = "User added successfully";
-    responseObj.data = userObj;
-    responseObj.status = 201;
+      //Emiting the event of success user added
+      req.io.emit("user-logged", responseObj);
+      res.send(responseObj);
+    } else {
+      await userObj.save();
 
-    //Emiting the event of success user added
-    req.io.emit("user-created", responseObj);
-    res.send(responseObj);
+      //Creating success object
+      responseObj.msg = "User added successfully";
+      responseObj.data = userObj;
+      responseObj.status = 201;
+
+      //Emiting the event of success user added
+      req.io.emit("user-created", responseObj);
+      res.send(responseObj);
+    }
   } catch (error) {
     //Creating success object
     responseObj.msg = "User adding failed";
