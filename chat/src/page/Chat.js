89cd1f05b6ac35c7389e8b19696afcfc8c3eea8c
sonangrai <shonahangrae@gmail.com>
2021-12-29 08:50:21 +0545

    client connect changhed

--- chat/src/page/Chat.js
+++ chat/src/page/Chat.js
@@ -17,11 +17,13 @@ const Chat = ({ user, getname }) => {
   let chatRef = useRef();
   const [msg, setmsg] = useState();
   const [socket, setsocket] = useState();
-  const newSocket = io(
-    process.env.NODE_ENV === "development"
-      ? process.env.REACT_APP_DEV_SOCKET
-      : process.env.REACT_APP_PROD_SOCKET
-  );
+  const newSocket = io({
+    path:
+      process.env.NODE_ENV === "development"
+        ? process.env.REACT_APP_DEV_SOCKET
+        : process.env.REACT_APP_PROD_SOCKET,
+    transports: ["websocket"],
+  });
   useEffect(() => {
     setsocket(newSocket);
     newSocket.emit("connected", user);
